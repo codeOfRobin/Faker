69502011e7520b45442892f016e407f5077056f2
Paul Calnan <me@paulcalnan.com>
2017-01-05 16:41:09 -0500

    Fixed shuffle bug

--- Source/Extensions/Array.swift
+++ Source/Extensions/Array.swift
@@ -25,11 +25,17 @@ import Foundation
 public extension Array {
     /**
         Shuffle the array in-place using the Fisher-Yates algorithm.
-    */
+     */
+    // via http://stackoverflow.com/a/24029847/511287
     public mutating func shuffle() {
-        for i in 0..<(count - 1) {
-            let j = Int(arc4random_uniform(UInt32(count - i))) + i
-            swap(&self[i], &self[j])
+        let c = count
+        guard c > 1 else { return }
+
+        for (firstUnshuffled , unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+            let d: IndexDistance = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+            guard d != 0 else { continue }
+            let i = index(firstUnshuffled, offsetBy: d)
+            swap(&self[firstUnshuffled], &self[i])
         }
     }
     
@@ -38,12 +44,12 @@ public extension Array {
         algorithm.
     
         - returns: Returns a shuffled version of the array.
-    */
+     */
+    // via http://stackoverflow.com/a/24029847/511287
     public func shuffled() -> [Element] {
-        var list = self
-        list.shuffle()
-        
-        return list
+        var result = Array(self)
+        result.shuffle()
+        return result
     }
     
     /**
